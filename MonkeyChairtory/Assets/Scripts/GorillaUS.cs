ab4c4a14c74bf7c26722dc4390d332831062ea8c
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2021-12-07 15:50:19 +0100

    Added basic utility curves

--- MonkeyChairtory/Assets/Scripts/GorillaUS.cs
+++ MonkeyChairtory/Assets/Scripts/GorillaUS.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GorillaUS : MonoBehaviour
 {
@@ -8,16 +9,74 @@ public class GorillaUS : MonoBehaviour
     public bool isTrapped;
     public bool isJailed;
 
+    [Header("Hunger config")]
+    [SerializeField] private float minRandomHunger = 30;
+    [SerializeField] private float maxRandomHunger = 40;
+    [SerializeField] private float maxSelectedHunger = 0;
+    [SerializeField] private float currentHunger = 0;
+
+
+    private UtilitySystemEngine gorillaCurves;
+    private BehaviourTreeEngine hungerBehaviour;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        SetRandomHunger();
+
+        gorillaCurves = new UtilitySystemEngine();
+        hungerBehaviour = new BehaviourTreeEngine();
+        CreateUtilityCurves();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gorillaCurves.Update();
+    }
+
+    void SetRandomHunger()
+    {
+        maxSelectedHunger = UnityEngine.Random.Range(minRandomHunger, maxRandomHunger);
+
+        maxSelectedHunger = 40;
+    }
+
+    void CreateUtilityCurves()
+    {
+        var world = FindObjectOfType<WorldManager>();
+
+        Factor leafLogsInTreatyZone = new LeafVariable(() => world.logsInTreatyZone, world.maxLogsInTreatyZone, 0);
+        Factor logsInTreatyZone = new LinearCurve(leafLogsInTreatyZone, -1, 1);
+
+        Factor leafPlanksInTreatyZone = new LeafVariable(() => world.planksInTreatyZone, world.maxPlanksInTreatyZone, 0);
+        Factor planksInTreatyZone = new LinearCurve(leafPlanksInTreatyZone);
+
+        Factor leafPlanksInMountingZone = new LeafVariable(() => world.planksInMountingZone, world.maxPlanksInMountingZone, 0);
+        Factor planksInMountingZone = new LinearCurve(leafPlanksInMountingZone, -1, 1);
+
+        //Debug.Log(logsInTreatyZone.getValue());
+
+        Factor leafTimeWithoutEating = new LeafVariable(() => currentHunger, maxSelectedHunger, 0);
+
+        List<Point2D> hungerGraphPoints = new List<Point2D>();
+        hungerGraphPoints.Add(new Point2D(0, 0));
+        hungerGraphPoints.Add(new Point2D(0.4f, 0.2f));
+        hungerGraphPoints.Add(new Point2D(0.6f, 0.8f));
+        hungerGraphPoints.Add(new Point2D(1, 1));
+
+        Factor timeWithoutEating = new LinearPartsCurve(leafTimeWithoutEating, hungerGraphPoints);
+
+        Debug.Log(timeWithoutEating.getValue());
+
+    }
+
+    void CreateBehaviourTree()
+    {
+
     }
 
     public void TrapGorilla(Transform patroller)
---
+++ MonkeyChairtory/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+    [Header("Logs config")]
+    public int logsInTreatyZone = 0;
+    public int maxLogsInTreatyZone = 50;
+
+    [Header("Planks treaty config")]
+    public int planksInTreatyZone = 0;
+    public int maxPlanksInTreatyZone = 400;
+
+    [Header("Planks mounting config")]
+    public int planksInMountingZone = 0;
+    public int maxPlanksInMountingZone = 400;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
