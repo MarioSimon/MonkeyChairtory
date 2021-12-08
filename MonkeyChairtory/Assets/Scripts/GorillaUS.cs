2e77a62d73671285a392fa631410e56aad234f5d
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2021-12-08 14:12:43 +0100

    Added basic gorilla utility system

--- MonkeyChairtory/Assets/Scripts/GorillaUS.cs
+++ MonkeyChairtory/Assets/Scripts/GorillaUS.cs
@@ -9,6 +9,12 @@ public class GorillaUS : MonoBehaviour
     public bool isTrapped;
     public bool isJailed;
 
+    [Header("Treaty zone config")]
+    [SerializeField] private bool isInTreatyZone;
+
+    [Header("Mounting zone config")]
+    [SerializeField] private bool isInMountingZone;
+
     [Header("Hunger config")]
     [SerializeField] private float minRandomHunger = 30;
     [SerializeField] private float maxRandomHunger = 40;
@@ -27,7 +33,7 @@ public class GorillaUS : MonoBehaviour
 
         SetRandomHunger();
 
-        gorillaCurves = new UtilitySystemEngine();
+        gorillaCurves = new UtilitySystemEngine(1.0f);
         hungerBehaviour = new BehaviourTreeEngine();
         CreateUtilityCurves();
     }
@@ -36,6 +42,19 @@ public class GorillaUS : MonoBehaviour
     void Update()
     {
         gorillaCurves.Update();
+
+        string aux = "[";
+
+        foreach (var action in gorillaCurves.actions)
+        {
+            aux += action.getUtility() + "/ ";
+        }
+
+        aux += "]";
+
+        Debug.Log(aux);
+
+        currentHunger += Time.deltaTime;
     }
 
     void SetRandomHunger()
@@ -58,7 +77,29 @@ public class GorillaUS : MonoBehaviour
         Factor leafPlanksInMountingZone = new LeafVariable(() => world.planksInMountingZone, world.maxPlanksInMountingZone, 0);
         Factor planksInMountingZone = new LinearCurve(leafPlanksInMountingZone, -1, 1);
 
-        //Debug.Log(logsInTreatyZone.getValue());
+        List<Factor> factors = new List<Factor>(){ planksInTreatyZone, planksInMountingZone };
+        Factor planksFromTreatingToMounting = new WeightedSumFusion(factors);
+
+        var monkeyTreatyAmt = MonkeysInTreatyZone();
+        Factor auxMonkeysInTreatyZone = new LeafVariable(() => monkeyTreatyAmt, 4, 0);
+        var monkeyTreatyNeed = (auxMonkeysInTreatyZone.getValue() + planksInTreatyZone.getValue()) / 2.0f;
+        Factor leafMonkeysInTreatyZone = new LeafVariable(() => monkeyTreatyNeed, 1, 0);
+
+        var monkeyMountingAmt = MonkeysInMountingZone();
+        Factor auxMonkeysInMountingZone = new LeafVariable(() => monkeyMountingAmt, 4, 0);
+        var monkeyMountingNeed = (auxMonkeysInMountingZone.getValue() + planksInMountingZone.getValue()) / 2.0f;
+        Factor leafMonkeysInMountingZone = new LeafVariable(() => monkeyMountingNeed, 1, 0);
+
+        List<Point2D> monkeyPoints = new List<Point2D>();
+        monkeyPoints.Add(new Point2D(0, 1));
+        monkeyPoints.Add(new Point2D(0.2f, 0.6f));
+        monkeyPoints.Add(new Point2D(0.4f, 0.4f));
+        monkeyPoints.Add(new Point2D(1, 0));
+
+        Factor monkeysInTreatyZoneNeed = new LinearPartsCurve(leafMonkeysInTreatyZone, monkeyPoints);
+        Factor monkeysInMountingZoneNeed = new LinearPartsCurve(leafMonkeysInMountingZone, monkeyPoints);
+
+
 
         Factor leafTimeWithoutEating = new LeafVariable(() => currentHunger, maxSelectedHunger, 0);
 
@@ -70,13 +111,44 @@ public class GorillaUS : MonoBehaviour
 
         Factor timeWithoutEating = new LinearPartsCurve(leafTimeWithoutEating, hungerGraphPoints);
 
-        Debug.Log(timeWithoutEating.getValue());
-
+        gorillaCurves.CreateUtilityAction("logs to treaty", () => { Debug.Log("Moving logs..."); }, logsInTreatyZone);
+        gorillaCurves.CreateUtilityAction("planks to mounting", () => { Debug.Log("Moving planks..."); }, planksFromTreatingToMounting);
+        gorillaCurves.CreateUtilityAction("treating logs", () => { Debug.Log("Treating logs..."); }, monkeysInTreatyZoneNeed);
+        gorillaCurves.CreateUtilityAction("mounting chairs", () => { Debug.Log("Mounting chairs..."); }, monkeysInMountingZoneNeed);
+        gorillaCurves.CreateUtilityAction("go eating", () => { Debug.Log("Going to eat..."); }, timeWithoutEating);
     }
 
     void CreateBehaviourTree()
     {
 
+    }
+
+    int MonkeysInTreatyZone()
+    {
+        int monkeyAmt = 0;
+
+        var monkeys = FindObjectsOfType<GorillaUS>();
+
+        foreach (var monkey in monkeys)
+        {
+            if (monkey.isInTreatyZone) monkeyAmt++;
+        }
+
+        return monkeyAmt;
+    }
+
+    int MonkeysInMountingZone()
+    {
+        int monkeyAmt = 0;
+
+        var monkeys = FindObjectsOfType<GorillaUS>();
+
+        foreach (var monkey in monkeys)
+        {
+            if (monkey.isInMountingZone) monkeyAmt++;
+        }
+
+        return monkeyAmt;
     }
 
     public void TrapGorilla(Transform patroller)
